55d1bc9789941225691edaef560881d4a8a1105d
vbeni30 <151450702+vbeni30@users.noreply.github.com>
2025-04-15 11:10:25 +0300

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,12 @@
 "use client"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes/dist/types"
+import type { ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  // Force light theme only
+  return (
+    <NextThemesProvider {...props} forcedTheme="light">
+      {children}
+    </NextThemesProvider>
+  )
 }
